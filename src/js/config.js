cd42b3904016180d179cb4cee94f995f36a6a5be
ftaskoski123 <filip.taskoski69@gmail.com>
2023-01-22 19:23:49 +0100

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f1fced79-df31-453d-b326-1464971f2780';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,18 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+import previewView from './previewView.js';
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+
+  _errorMessage = 'No recipes found for your query! Please try again!';
+  _message = '';
+
+  _generateMarkup() {
+    console.log(this._data);
+
+    return this._data.map(result => previewView.render(result, false)).join('');
+  }
+}
+
+export default new ResultsView();
